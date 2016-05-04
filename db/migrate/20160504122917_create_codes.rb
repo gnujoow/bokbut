65d0d0ebae02a02c0bb1977e3e729ffe1dc19a26
osx_gnujoow <gnujoow.kim@gmail.com>
2016-05-04 22:20:03 +0900

    db edited

--- db/migrate/20160504122917_create_codes.rb
+++ db/migrate/20160504122917_create_codes.rb
@@ -1,8 +1,11 @@
 class CreateCodes < ActiveRecord::Migration
   def change
     create_table :codes do |t|
-
-      t.timestamps null: false
+      t.integer :post_id         # 포스트아이디
+      t.string  :language        # 코드의 언어
+      t.string  :description     # 코드의 설명
+      t.text    :source          # 소스코드
+      t.timestamps               # 타임스탬프
     end
   end
 end
