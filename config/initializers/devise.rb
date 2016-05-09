400a12b48330c6d6e30a843cb523d4b3271d2d54
jisung0178 <tlliyun@naver.com>
2016-05-09 04:19:32 +0000

    facebook login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -265,4 +265,5 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.omniauth :facebook, "349271561863805", "9a4a492fa7e84a2da8efc0326ca3da36"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506101206) do
+ActiveRecord::Schema.define(version: 20160509035915) do
 
   create_table "codes", force: :cascade do |t|
     t.string   "language"
@@ -42,6 +42,10 @@ ActiveRecord::Schema.define(version: 20160506101206) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "oauth_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
